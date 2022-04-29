15301ebe973b486feaee7824604d71b314811942
Paulson McIntyre <pmcintyre@salesforce.com>
2022-04-29 07:34:54 -0400

    Vendor--

--- vendor/github.com/ptdave20/donordrive/badges.go
+++
@@ -1,9 +0,0 @@
-package donordrive
-
-type Badge struct {
-	BadgeCode       string `json:"badgeCode"`
-	BadgeImageURL   string `json:"badgeImageURL"`
-	Description     string `json:"description"`
-	Title           string `json:"title"`
-	UnlockedDateUTC string `json:"unlockedDateUTC"`
-}
